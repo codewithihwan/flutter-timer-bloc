02b61451a89ebb82bb95c19ba7d09ec859255f26
Ihwan <ihwan.id@icloud.com>
2023-07-30 21:11:36 +0700

    add bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,17 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_timer_bloc/app.dart';
 
 void main() {
+  Bloc.observer = SimpleBlocObserver();
   runApp(const App());
+}
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    // ignore: avoid_print
+    print(change.nextState.toString());
+  }
 }
